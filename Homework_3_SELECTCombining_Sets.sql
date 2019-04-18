7bb8de2f53d38599b6ec64b6d9dcce89b7f9813b
Roman Kuk <34649365+RomanCook@users.noreply.github.com>
2019-04-19 01:02:32 +0300

    Update Homework_3_SELECTCombining_Sets.sql

--- Homework_3_SELECTCombining_Sets.sql
+++ Homework_3_SELECTCombining_Sets.sql
@@ -6,3 +6,13 @@ go
 SELECT pr.model, pr.type, maker
 FROM printer pr JOIN Product_CTE ON Product_CTE.model = pr.model
 ORDER BY type;
+--CTE--05--
+use [labor_sql];
+go
+;WITH num_CTE(num)
+AS 
+(SELECT num = 1 
+UNION ALL
+SELECT num + 1 FROM num_CTE WHERE num < 10000)
+SELECT * FROM num_CTE
+OPTION (MAXRECURSION 10000);
